bf72a515bc9dc78ff864e08607dc4c82d6465deb
koorchik <koorchik@gmail.com>
2016-01-29 10:29:42 +0200

    Add validation and Timer

--- src/Timer.js
+++ src/Timer.js
@@ -1,7 +1,34 @@
 'use strict';
 
 class Timer {
+    constructor() {
+        this.spent = 0;
+        this.isActive = false;
+        this.lastSessionStartetAt = 0;
+    }
 
+    start() {
+        this.isActive  = true;
+        this.lastSessionStartetAt = new Date().getTime();
+    }
+
+    stop() {
+        if (! this.isActive ) return;
+
+        this.isActive = false;
+        this.spent += new Date().getTime() - this.lastSessionStartetAt;
+        this.lastSessionStartetAt = 0;
+    }
+
+    getSpentTime() {
+        if (!this.isActive) return this.spent;
+        return this.spent + (new Date().getTime() - this.lastSessionStartetAt);
+    }
+
+    clear() {
+        this.lastSessionStartetAt = new Date().getTime();
+        this.spent = 0;
+    }
 }
 
 module.exports = Timer;
